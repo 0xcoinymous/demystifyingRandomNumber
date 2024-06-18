9b73eabc0dd6f490a4d3258b83b54857007b7953
Abolfazl <ab.iraninasab.crypto1400@gmail.com>
2024-06-18 11:03:03 +0330

    add localhost to hardaht configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,17 @@ require("hardhat-gas-reporter");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: {
+        defaultNetwork: "localhost",
+        networks:{
+            localhost: {
+                url: "http://127.0.0.1:8545/",
+                // mining: {
+                //     auto: false,
+                //     interval: 0
+                // },
+                // blockGasLimit: 300000000
+            }
+        },
         version: "0.8.19",
         settings: {
             // optimizer: {
